ccdf60c11f0706e433761834b7ed71878fe9fbd6
Pulkit <pulkit.summers@gmail.com>
2023-11-30 19:43:31 +0530

    Updates

--- src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-    baseURL: "https://api.quicksell.co/v1/internal"
-})
-
-export const API = {
-    getData: async () => api.get('/frontend-assignment')
-}
